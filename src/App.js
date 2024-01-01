dc3f440023c26658f745cf6d708e9b33053305c5
Shaha-88 <shaha1988@hotmail.com>
2024-01-01 11:30:39 +0300

    task 1 shahashop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hellooo</h1>
+      <h1>this is my website task1</h1>
+      <p>this is the solution of the first task</p>
+      <img src="/market.jpg" alt="error" />
     </div>
   );
 }
